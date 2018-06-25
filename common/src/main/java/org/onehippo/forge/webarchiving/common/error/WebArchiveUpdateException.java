106e751dcb0687e3f64c9329e39af38d894d64a6
mchatzidakis <minos.chatzidakis@bloomreach.com>
2018-06-25 19:33:05 +0200

    minors

---
+++ common/src/main/java/org/onehippo/forge/webarchiving/common/error/WebArchiveUpdateException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 BloomReach Inc. (http://www.bloomreach.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onehippo.forge.webarchiving.common.error;
+
+/**
+ * The <CODE>WebArchiveUpdateException</CODE> class defines a general exception
+ * that the web archiving module can throw when it is unable to perform its operation
+ * successfully.
+ */
+public class WebArchiveUpdateException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public WebArchiveUpdateException(final String message) {
+        super(message);
+    }
+
+    public WebArchiveUpdateException(final Throwable cause) {
+        super(cause);
+    }
+
+    public WebArchiveUpdateException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+}
