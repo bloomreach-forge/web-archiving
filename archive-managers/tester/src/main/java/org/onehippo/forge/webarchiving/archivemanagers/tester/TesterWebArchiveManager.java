53558de3b104fe139b3564dcac0cf647336fa575
mchatzidakis <minos.chatzidakis@bloomreach.com>
2018-07-03 20:53:22 +0200

    multithreading

--- archive-managers/tester/src/main/java/org/onehippo/forge/webarchiving/archivemanagers/tester/TesterWebArchiveManager.java
+++ archive-managers/tester/src/main/java/org/onehippo/forge/webarchiving/archivemanagers/tester/TesterWebArchiveManager.java
@@ -16,7 +16,6 @@
 
 package org.onehippo.forge.webarchiving.archivemanagers.tester;
 
-import java.util.Calendar;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
