a6bb40408c42d54fd69cf4dd8679eb0a88e65bf6
mchatzidakis <minos.chatzidakis@bloomreach.com>
2018-07-03 13:19:32 +0200

    tester archive manager

--- archive-managers/tester/src/main/java/org/onehippo/forge/webarchiving/archivemanagers/tester/TesterWebArchiveManager.java
+++ archive-managers/tester/src/main/java/org/onehippo/forge/webarchiving/archivemanagers/tester/TesterWebArchiveManager.java
@@ -17,7 +17,10 @@
 package org.onehippo.forge.webarchiving.archivemanagers.tester;
 
 import java.util.Calendar;
+import java.util.Map;
 
+import org.onehippo.forge.webarchiving.cms.util.Discoverable;
+import org.onehippo.forge.webarchiving.cms.util.LifeCycle;
 import org.onehippo.forge.webarchiving.common.api.WebArchiveManager;
 import org.onehippo.forge.webarchiving.common.error.WebArchiveUpdateException;
 import org.onehippo.forge.webarchiving.common.model.WebArchiveUpdate;
@@ -25,18 +28,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class TesterWebArchiveManager implements WebArchiveManager {
+public class TesterWebArchiveManager implements WebArchiveManager, LifeCycle, Discoverable {
     private static final Logger log = LoggerFactory.getLogger(TesterWebArchiveManager.class);
+
+    private int testerSleepTimeInSeconds = 10;
+
+    @Override
+    public synchronized void initialize(final Map<String, String> props) {
+        final String sleepSeconds = props.get("tester.sleep.seconds");
+        if (sleepSeconds != null) {
+            try {
+                testerSleepTimeInSeconds = Integer.parseInt(sleepSeconds);
+            } catch (NumberFormatException nfe) {
+                log.error("Initializing {}: cannot parse property tester.sleep.seconds='{}' into integer", this.getClass().getName(), sleepSeconds);
+            }
+        }
+        log.info("Initialized {}. Simulating a Web Archive service with conf: sleep seconds {}", this.getClass().getName(), testerSleepTimeInSeconds);
+    }
+
+    @Override
+    public void destroy() {
+        log.debug("Destroying {}", this.getClass().getName());
+    }
 
     @Override
     public void requestUpdate(final WebArchiveUpdate update) throws WebArchiveUpdateException {
-        if (Calendar.getInstance().get(Calendar.MINUTE) % 2 != 0) {
+        try {
+            Thread.sleep(testerSleepTimeInSeconds * 1000);
+        } catch (InterruptedException e) {
+        }
+        if (Calendar.getInstance().get(Calendar.SECOND) % 2 != 0) {
             throw new WebArchiveUpdateException("It's an odd minute, web archive is closed now");
         }
     }
 
     @Override
     public String getArchiveManagerInfo() {
-        return "Tester Web Archive Manager [" + this.getClass().getSimpleName() +"]";
+        return "Tester Web Archive Manager [" + this.getClass().getSimpleName() + "]";
     }
 }
