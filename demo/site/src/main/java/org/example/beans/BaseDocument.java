e121506c5e534be83a3b97557f970868db12aa5a
mchatzi <minos.chatzidakis@bloomreach.com>
2019-08-30 11:06:22 +0200

    Removed v12 demo

--- demo/site/src/main/java/org/example/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.example.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="webarchivingaddondemov12:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
