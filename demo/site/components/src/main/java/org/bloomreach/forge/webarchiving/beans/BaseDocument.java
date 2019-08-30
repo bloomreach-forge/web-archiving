23a32aa5f44a3a8eb1400d5c71529db8d27eab22
mchatzi <minos.chatzidakis@bloomreach.com>
2019-08-30 14:32:16 +0200

    Upgrade to v13, demo, wip

---
+++ demo/site/components/src/main/java/org/bloomreach/forge/webarchiving/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.bloomreach.forge.webarchiving.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="webarchivingaddondemov13:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
