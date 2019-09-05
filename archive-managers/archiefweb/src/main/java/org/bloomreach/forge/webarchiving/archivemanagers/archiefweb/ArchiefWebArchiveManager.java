709479b0bd8d98998adf1b9f220885e74dd9d1f9
mchatzi <minos.chatzidakis@bloomreach.com>
2019-09-05 11:54:23 +0200

    Using ArchiefWeb manager
    CRISP
    
    Configurable ArchiefWebArchiveManager using CRISP

--- archive-managers/archiefweb/src/main/java/org/bloomreach/forge/webarchiving/archivemanagers/archiefweb/ArchiefWebArchiveManager.java
+++ archive-managers/archiefweb/src/main/java/org/bloomreach/forge/webarchiving/archivemanagers/archiefweb/ArchiefWebArchiveManager.java
@@ -23,6 +23,14 @@ import org.bloomreach.forge.webarchiving.cms.util.PlatformManaged;
 import org.bloomreach.forge.webarchiving.common.api.WebArchiveManager;
 import org.bloomreach.forge.webarchiving.common.error.WebArchiveUpdateException;
 import org.bloomreach.forge.webarchiving.common.model.WebArchiveUpdate;
+import org.hippoecm.hst.site.HstServices;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.onehippo.cms7.crisp.api.broker.ResourceServiceBroker;
+import org.onehippo.cms7.crisp.api.exchange.ExchangeHintBuilder;
+import org.onehippo.cms7.crisp.api.resource.Resource;
+import org.onehippo.cms7.crisp.hst.module.CrispHstServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,8 +38,34 @@ import org.slf4j.LoggerFactory;
 public class ArchiefWebArchiveManager implements WebArchiveManager, PlatformManaged, Discoverable {
     private static final Logger log = LoggerFactory.getLogger(ArchiefWebArchiveManager.class);
 
+    protected static final String CONFIG_PROP_AUTH_ENDPOINT = "archiefweb.api.endpoint.auth";
+    protected static final String CONFIG_PROP_REQUEST_ENDPOINT = "archiefweb.api.endpoint.request";
+    protected static final String CONFIG_PROP_USERNAME = "archiefweb.api.username";
+    protected static final String CONFIG_PROP_PASSWORD = "archiefweb.api.password";
+
+    private String authEndpoint;
+    private String requestEndpoint;
+    private String username;
+    private String password;
+
     @Override
     public void initialize(final Map<String, String> props) throws WebArchiveUpdateException {
+        authEndpoint = props.get(CONFIG_PROP_AUTH_ENDPOINT);
+        if (authEndpoint == null) {
+            throw new WebArchiveUpdateException("Please configure the authentication endpoint (" + CONFIG_PROP_AUTH_ENDPOINT + ")");
+        }
+        requestEndpoint = props.get(CONFIG_PROP_REQUEST_ENDPOINT);
+        if (requestEndpoint == null) {
+            throw new WebArchiveUpdateException("Please configure the request endpoint (" + CONFIG_PROP_REQUEST_ENDPOINT + ")");
+        }
+        username = props.get(CONFIG_PROP_USERNAME);
+        if (username == null) {
+            throw new WebArchiveUpdateException("Please configure api username (" + CONFIG_PROP_USERNAME + ")");
+        }
+        password = props.get(CONFIG_PROP_PASSWORD);
+        if (password == null) {
+            throw new WebArchiveUpdateException("Please configure api password (" + CONFIG_PROP_PASSWORD + ")");
+        }
     }
 
     @Override
@@ -41,12 +75,51 @@ public class ArchiefWebArchiveManager implements WebArchiveManager, PlatformMana
 
     @Override
     public synchronized void requestUpdate(final WebArchiveUpdate update) throws WebArchiveUpdateException {
+        ResourceServiceBroker broker = CrispHstServices.getDefaultResourceServiceBroker(HstServices.getComponentManager());
+        Resource authInfo = broker.resolve("archiefWebEndpoint", authEndpoint,
+                ExchangeHintBuilder.create()
+                        .methodName("POST")
+                        .requestHeader("Content-Type", "application/json;charset=UTF-8")
+                        .requestBody("{\"email\":\"" + username + "\",\n" +
+                                " \"password\":\"" + password + "\"}")
+                        .build());
 
+        if ("Authentication successful.".equals(authInfo.getValue("message"))) {
+            String jwtToken = authInfo.getValue("jwt", String.class);
+            Resource operationResource = broker.resolve("archiefWebEndpoint", requestEndpoint,
+                    ExchangeHintBuilder.create()
+                            .methodName("POST")
+                            .requestHeader("Content-Type", "application/json;charset=UTF-8")
+                            .requestHeader("Authorization", "Bearer " + jwtToken)
+                            .requestBody(createRequestBody(update))
+                            .build());
+
+            System.out.println(operationResource.getValue("message"));
+            if ("Action completed successfully.".equals(operationResource.getValue("message"))) {
+                System.out.println("SUCCESS!!");
+            } else {
+                throw new WebArchiveUpdateException("Web archive update failed " + update);
+            }
+
+        } else {
+            throw new WebArchiveUpdateException("Authentication failed, web archive update " + update);
+        }
     }
 
-    @Override
-    public String getArchiveManagerInfo() {
-        return this.getClass().getSimpleName();
+    private String createRequestBody(final WebArchiveUpdate update) throws WebArchiveUpdateException {
+        JSONObject payload = new JSONObject();
+        try {
+            payload.put("url", new JSONArray(update.getUrls()));
+            payload.put("action", "add");
+        } catch (JSONException e) {
+            throw new WebArchiveUpdateException("Error while creating web archive update request", e);
+        }
 
+
+        return payload.toString();
+        /*return "{\"url\":\n" +
+                " [\"https://www.politie.nl/aangifte-of-meldingdoen\",\"https://www.politie.nl/mijnbuurt\"],\n" +
+                " \"action\":\"add\"\n" +
+                "}";*/
     }
 }
